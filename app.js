5fe3624083bc3e1a32e2b41ac0b469fd0d4b230e
Abheekrishna <abheekrishna17@gmail.com>
2023-06-23 18:52:40 +0530

    Fixed bug in equals btn and completed mathOperations

--- app.js
+++ app.js
@@ -17,14 +17,68 @@ let lastOperation = '';
 
 numberBtn.forEach(number => {
     number.addEventListener('click', (e) => {
-        console.log(!haveDot)
         if(e.target.innerText === '.' && !haveDot) {
             haveDot = true;
         } else if(e.target.innerText === '.' && haveDot) {
             return;
         }
         currentNumber += e.target.innerText;
-        currentNumberTextDiv.innerText = currentNumber; 
-
+        currentNumberTextDiv.innerText = currentNumber;
     })
 })
+
+operationsBtn.forEach(operation => {
+    operation.addEventListener('click', (e) => {
+        const operationName = e.target.innerText;
+        if(!currentNumber) return;
+        haveDot = false;
+        if(currentNumber && lastOperation && previousNumber) {
+            mathOperations();
+        } else {
+            result  = parseFloat(currentNumber);
+        }
+        
+        lastOperation = operationName;
+        clear(lastOperation);
+    })
+})
+
+const mathOperations = () => {
+    if(lastOperation === '/') {
+        result = parseFloat(result) / parseFloat(currentNumber);
+    } else if(lastOperation === '*') {
+        result = parseFloat(result) * parseFloat(currentNumber);
+    } else if (lastOperation === '-') {
+        result = parseFloat(result) - parseFloat(currentNumber);
+    } else if (lastOperation === '+') {
+        result = parseFloat(result) + parseFloat(currentNumber);
+    }
+}
+
+const clear = (operationName = '') => {
+    previousNumber = `${currentNumber} ${operationName}`;
+    previousNumberTextDiv.innerText = previousNumber;
+    currentNumber = '';
+    currentNumberTextDiv.innerText = '';
+}
+
+equalsBtn.addEventListener('click', () => {
+    mathOperations();
+    clear(lastOperation);
+    currentNumberTextDiv.innerText = result;
+})
+
+allClearBtn.addEventListener('click', () => {
+    currentNumberTextDiv.innerText = '';
+    previousNumberTextDiv.innerText = '';
+    currentNumber = '';
+    lastOperation = '';
+    previousNumber = '';
+    reset()
+})
+
+const reset = () => {
+    equalsBtn.addEventListener('click' , () => {
+        currentNumberTextDiv.innerText = '';
+    })
+}
